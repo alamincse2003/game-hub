5dc58cd40fd1e061f82c8f20c4f5871fee5d250d
alamincse2003 <mdalamincse2003@gmail.com>
2026-01-09 11:47:41 +0600

    refactoring duplicate remove interface

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -8,8 +8,7 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
