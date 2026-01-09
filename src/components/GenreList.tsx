4f051b12744f59de8567fbc15e99dcc187bc7f28
alamincse2003 <mdalamincse2003@gmail.com>
2026-01-09 10:36:37 +0600

    useData hooks remove and api-client from all data render

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,8 +7,8 @@ import {
   ListItem,
   Spinner,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import useGenres from "../hooks/useGenres";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
