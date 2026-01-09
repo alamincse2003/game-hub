f0d8c17da32bd045516ae1a7d2082aa4105cd66c
alamincse2003 <mdalamincse2003@gmail.com>
2026-01-09 18:58:58 +0600

    Refactor - crate lookup hooks

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -9,6 +9,7 @@ import {
 } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
@@ -18,9 +19,10 @@ interface Props {
 const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error, isLoading } = usePlatforms();
 
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  );
+  const selectedPlatform = usePlatform(selectedPlatformId);
+  // const selectedPlatform = data?.results.find(
+  //   (p) => p.id === selectedPlatformId
+  // );
 
   if (isLoading) {
     return (
